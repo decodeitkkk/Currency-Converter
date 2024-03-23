0a2db8b21f00bf68e2ad21ede1e4d47712d09200
decodeitkkk <kkailash0086@gmail.com>
2024-03-23 14:01:25 +0530

    api chnaged

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import useCurrencyInfo from "./hooks/useCurrencyInfo";
 function App() {
     const [amount, setAmount] = useState(1);
     const [convertedAmount, setConvertedAmount] = useState(0);
-    const [from, setFrom] = useState("usd");
-    const [to, setTo] = useState("inr");
+    const [from, setFrom] = useState("USD");
+    const [to, setTo] = useState("INR");
 
     let currencyOptionsFromAPI = useCurrencyInfo(from);
 
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -7,11 +7,12 @@ function useCurrencyInfo(currency) {
         let fetchData = async () => {
             try {
                 let r = await fetch(
-                    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+                    `https://open.er-api.com/v6/latest/${currency}`
                 );
                 let res = await r.json();
                 // console.log(`before `,data)
-                setData(res[currency]);
+                // setData(res[currency]);
+                setData(res.rates)
                 // console.log(`after `,data)
             } catch (error) {
                 console.error("Error fetching data:", error);
